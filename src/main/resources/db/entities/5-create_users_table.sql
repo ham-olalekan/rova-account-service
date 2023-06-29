53526057bebf1deffa9da20fa2e658e54c5ba21e
Olalekan Lukman <olalekanl@Olalekans-MacBook-Pro.local>
2023-06-29 02:31:13 +0100

    setup user and bank account tables

---
+++ src/main/resources/db/entities/5-create_users_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(20) NOT NULL,
+    middle_name VARCHAR(20) NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    country_code VARCHAR(5) NULL,
+    phone_no VARCHAR(20) NOT NULL UNIQUE,
+    image_url VARCHAR(255) NULL,
+    enabled BOOLEAN NOT NULL DEFAULT TRUE,
+    email_verified BOOLEAN NOT NULL,
+    phone_no_verified BOOLEAN NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    INDEX index_email (email),
+    INDEX index_username (username),
+    INDEX index_phone_no (phone_no)
+)
